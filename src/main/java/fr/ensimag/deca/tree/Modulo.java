f8b748fa71fecae8daa2cfc62eea8c2f8107d591
Willem <logan.willem@grenoble-inp.org>
2023-01-09 20:19:24 +0100

    Small correction for the error message

--- src/main/java/fr/ensimag/deca/tree/Modulo.java
+++ src/main/java/fr/ensimag/deca/tree/Modulo.java
@@ -25,7 +25,7 @@ public class Modulo extends AbstractOpArith {
         Location loc = this.getLocation();
 
         if (!typeLeft.isInt() || !typeRight.isInt())
-            throw new ContextualError("A modulo is only between 2 int (rule 3.33)", loc);
+            throw new ContextualError("A modulo can only be done between 2 int (rule 3.33)", loc);
         
         // Ajout du décor
         this.setType(typeLeft);
