efe7f4edd4e712d795561045fb675316bbe85cd1
Eclipse <virgile.henry0211@gmail.com>
2023-01-10 17:15:23 +0100

    added no op

--- src/main/java/fr/ensimag/deca/tree/NoOperation.java
+++ src/main/java/fr/ensimag/deca/tree/NoOperation.java
@@ -23,7 +23,7 @@ public class NoOperation extends AbstractInst {
 
     @Override
     protected void codeGenInst(DecacCompiler compiler) {
-        throw new UnsupportedOperationException("not yet implemented");
+        // nothing to do...
     }
 
     @Override
