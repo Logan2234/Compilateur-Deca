e1437b46daa8440155ad6f54c00892b143606c6f
Nils DEPUILLE <nils.depuille@grenoble-inp.org>
2023-01-11 14:49:05 +0100

    Ajout Test unitaire Not et multiple

---
+++ src/test/java/fr/ensimag/deca/context/TestNotAdvanced.java
@@ -0,0 +1,45 @@
+package fr.ensimag.deca.context;
+
+import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.tree.AbstractExpr;
+import fr.ensimag.deca.tree.ConvFloat;
+import fr.ensimag.deca.tree.Plus;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+import static org.mockito.Mockito.*;
+import org.mockito.MockitoAnnotations;
+
+/**
+ * Test for the Plus node using mockito, using @Mock and @Before annotations.
+ *
+ * @author Ensimag
+ * @date 01/01/2023
+ */
+public class TestPlusAdvanced {
+
+    final Type BOOLEAN = new BooleanType(null);
+
+    @Mock
+    AbstractExpr booleanexpr;
+
+    DecacCompiler compiler;
+    
+    @BeforeEach
+    public void setup() throws ContextualError {
+        MockitoAnnotations.initMocks(this);
+        compiler = new DecacCompiler(null, null);
+        when(bolleanexpr.verifyExpr(compiler, null, null)).thenReturn(BOOLEAN);
+
+    }
+
+    @Test
+    public void testBoolean() throws ContextualError {
+        Plus t = new Not(booleanexpr);
+        // check the result
+        assertTrue(t.verifyExpr(compiler, null, null).isBoolean());
+        // check that the mocks have been called properly.
+        verify(booleanexpr).verifyExpr(compiler, null, null);
+    }
+}
