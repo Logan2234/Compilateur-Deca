0497246aafd91e20afe7753904df76efb418b96c
Nils DEPUILLE <nils.depuille@grenoble-inp.org>
2023-01-19 11:16:15 +0100

    Debut du developpement du multiply pour comprendre comment ca marche

--- src/main/java/fr/ensimag/deca/tree/Multiply.java
+++ src/main/java/fr/ensimag/deca/tree/Multiply.java
@@ -2,6 +2,7 @@ package fr.ensimag.deca.tree;
 
 import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.deca.codegen.runtimeErrors.OpOverflowErr;
+import fr.ensimag.deca.context.Type;
 import fr.ensimag.ima.pseudocode.DVal;
 import fr.ensimag.ima.pseudocode.GPRegister;
 import fr.ensimag.ima.pseudocode.instructions.BOV;
@@ -36,6 +37,19 @@ public class Multiply extends AbstractOpArith {
 
     @Override
     public boolean factorised() {
-        return false;//TODO
+        if (leftOperand.isLiteral() ^ rightOperand.isLiteral()) {
+            if (leftOperand.isLiteral() && leftOperand.getType().isInt())
+                if (((IntLiteral) leftOperand).getValue() < 4)
+                    //Developper en somme
+                    return true;
+            if (leftOperand.isLiteral() && leftOperand.getType().isFloat())
+                return false; // On ne developpe pas les floats en somme car il faudra dans tous les cas faire des additions donc pas optim
+            if (rightOperand.getType().isInt())
+                if (((IntLiteral) rightOperand).getValue() < 4)
+                        //Developper en somme
+                        return true;
+            return false;
+        }
+        return false;
     }
 }
