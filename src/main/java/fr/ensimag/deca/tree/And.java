69daa21e297f2a089a37b45e2bb31bc4583c97db
Eclipse <virgile.henry0211@gmail.com>
2023-01-21 19:22:42 +0100

    added lazy evaluation of bin exp

--- src/main/java/fr/ensimag/deca/tree/And.java
+++ src/main/java/fr/ensimag/deca/tree/And.java
@@ -3,7 +3,10 @@ package fr.ensimag.deca.tree;
 import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.ima.pseudocode.DVal;
 import fr.ensimag.ima.pseudocode.GPRegister;
+import fr.ensimag.ima.pseudocode.Label;
 import fr.ensimag.ima.pseudocode.instructions.ADD;
+import fr.ensimag.ima.pseudocode.instructions.BEQ;
+import fr.ensimag.ima.pseudocode.instructions.CMP;
 import fr.ensimag.ima.pseudocode.instructions.SHR;
 import fr.ensimag.ima.pseudocode.instructions.SNE;
 
@@ -30,4 +33,12 @@ public class And extends AbstractOpBool {
         compiler.addInstruction(new SHR(register)); // raise flags for CMP #0, val
         compiler.addInstruction(new SNE(register)); // so true if neq (false if eq)
     }
+
+    @Override
+    public void lazyEvaluation(DecacCompiler compiler, GPRegister resultRegister, Label toLabel) {
+        // and : if the result of register is false, branch to label
+        compiler.addInstruction(new CMP(0, resultRegister));
+        // if result register contains zero, return false in it 
+        compiler.addInstruction(new BEQ(toLabel));
+    } 
 }
--- src/main/java/fr/ensimag/deca/tree/Or.java
+++ src/main/java/fr/ensimag/deca/tree/Or.java
@@ -3,7 +3,10 @@ package fr.ensimag.deca.tree;
 import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.ima.pseudocode.DVal;
 import fr.ensimag.ima.pseudocode.GPRegister;
+import fr.ensimag.ima.pseudocode.Label;
 import fr.ensimag.ima.pseudocode.instructions.ADD;
+import fr.ensimag.ima.pseudocode.instructions.BEQ;
+import fr.ensimag.ima.pseudocode.instructions.CMP;
 import fr.ensimag.ima.pseudocode.instructions.SNE;
 
 /**
@@ -29,5 +32,11 @@ public class Or extends AbstractOpBool {
         compiler.addInstruction(new SNE(register));
     }
 
-
+    @Override
+    public void lazyEvaluation(DecacCompiler compiler, GPRegister resultRegister, Label toLabel) {
+        // and : if the result of register is false, branch to label
+        compiler.addInstruction(new CMP(1, resultRegister));
+        // if result register contains zero, return false in it 
+        compiler.addInstruction(new BEQ(toLabel));
+    } 
 }
